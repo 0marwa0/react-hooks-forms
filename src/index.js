169c6693e317f8733dd03c6b3d065e928d975342
marwa <marwajawad19@gmail.com>
2021-08-01 05:58:58 +0300

    Adding ParentComponent and DisplayData component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/ParentComponet';
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
